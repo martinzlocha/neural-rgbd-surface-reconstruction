b440f1f697320ddd3201bfefcd9a1c7cd1359a2d
Martin Zlocha <zlocha.martin@gmail.com>
2022-10-23 13:11:20 +0000

    Trial Record3D loader.

--- load_dataset.py
+++ load_dataset.py
@@ -1,4 +1,5 @@
 from load_scannet import load_scannet_data
+from load_record3d import load_record3d_data
 
 
 def load_dataset(args):
@@ -12,6 +13,17 @@ def load_dataset(args):
                                                                             crop=args.crop)
 
         print('Loaded scannet', images.shape, hwf, args.datadir)
+
+    elif args.dataset_type == "record3d":
+
+        images, depth_images, poses, hwf, frame_indices = load_record3d_data(basedir=args.datadir,
+                                                                            trainskip=args.trainskip,
+                                                                            downsample_factor=args.factor,
+                                                                            translation=args.translation,
+                                                                            sc_factor=args.sc_factor,
+                                                                            crop=args.crop)
+
+        print('Loaded Record3D', images.shape, hwf, args.datadir)
 
     # Calls to other dataloaders go here
     # elif args.dataset_type == "":
---
+++ load_record3d.py
@@ -0,0 +1,80 @@
+import os
+import imageio
+import json
+import cv2
+from pyquaternion import Quaternion
+from dataloader_util import *
+
+
+def load_record3d_data(basedir, trainskip, downsample_factor=1, translation=0.0, sc_factor=1., crop=0):
+    # Get image filenames, poses and intrinsics
+    img_files = [f for f in sorted(os.listdir(os.path.join(basedir, 'rgb')), key=alphanum_key) if f.endswith('jpg')]
+    depth_files = [f for f in sorted(os.listdir(os.path.join(basedir, 'depth')), key=alphanum_key) if f.endswith('exr')]
+
+    with open(os.path.join(basedir, 'metadata.json'), 'r') as f:
+        metadata = json.load(f)
+
+    # Train, val and test split
+    num_frames = len(img_files)
+    train_frame_ids = list(range(0, num_frames, trainskip))
+
+    # Lists for the data to load into
+    images = []
+    depth_maps = []
+    poses = []
+    frame_indices = []
+
+    # Read images and depth maps for which valid poses exist
+    for i in train_frame_ids:
+        img = imageio.imread(os.path.join(basedir, 'rgb', img_files[i]))
+        depth = cv2.imread(os.path.join(basedir, 'depth', depth_files[i]), -1)
+        depth = depth[:, :, 2]
+
+        pose_arr = metadata['poses'][i]
+        quat = Quaternion(pose_arr[:4])
+        pose_matrix = quat.transformation_matrix
+        pose_matrix[:3, 3] = pose_arr[4:]
+
+        images.append(img)
+        depth_maps.append(depth)
+        poses.append(pose_matrix)
+        frame_indices.append(i)
+
+    # Map images to [0, 1] range
+    images = (np.array(images) / 255.).astype(np.float32)
+
+    depth_maps = np.array(depth_maps).astype(np.float32)
+    depth_maps *= sc_factor
+    depth_maps = depth_maps[..., np.newaxis]
+
+    poses = np.array(poses).astype(np.float32)
+    poses[:, :3, 3] += translation
+    poses[:, :3, 3] *= sc_factor
+
+    # Intrinsics
+    H, W = depth_maps[0].shape[:2]
+    focal = metadata['K'][0]
+
+    # Resize color frames to match depth
+    images = resize_images(images, H, W)
+
+    # Crop the undistortion artifacts
+    if crop > 0:
+        images = images[:, crop:-crop, crop:-crop, :]
+        depth_maps = depth_maps[:, crop:-crop, crop:-crop, :]
+        H, W = depth_maps[0].shape[:2]
+
+    if downsample_factor > 1:
+        H = H//downsample_factor
+        W = W//downsample_factor
+        focal = focal/downsample_factor
+        images = resize_images(images, H, W)
+        depth_maps = resize_images(depth_maps, H, W, interpolation=cv2.INTER_NEAREST)
+
+    print(f'Image shape: {images.shape}')
+    print(f'Depth map shape: {depth_maps.shape}')
+    print(f'Depth min: {np.min(depth_maps)}')
+    print(f'Depth max: {np.max(depth_maps)}')
+    print(f'Poses: {poses.shape}')
+
+    return images, depth_maps, poses, [H, W, focal], frame_indices
